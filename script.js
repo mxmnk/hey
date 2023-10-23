5cb49f099555afd430c5b2f1452f1dcdf582e625
mxmnk <46030850+mxmnk@users.noreply.github.com>
2023-10-23 16:18:27 +0300

    Update PWA

--- script.js
+++ script.js
@@ -1,60 +1,37 @@
-// // Don't register the service worker
-// // until the page has fully loaded
-// window.addEventListener('load', () => {
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//       .register('/sw.js')
-//       .then((registration) => {
-//         console.log('Service worker registered!', registration);
-//       })
-//       .catch((error) => {
-//         console.warn('Error registering service worker:');
-//         console.warn(error);
-//       });
-//   }
-// });
-
+// Don't register the service worker
+// until the page has fully loaded
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        return registration.pushManager
+          .getSubscription()
+          .then(async (subscription) => {
+            // registration part
+          });
+      })
+      .then((registration) => {
+        console.log('Service worker registered!', registration);
+      })
+      .catch((error) => {
+        console.warn('Error registering service worker:');
+        console.warn(error);
+      });
   }
 });
 
 const button = document.getElementById('notifications');
 
-function randomNotification() {
-  const notifBody = `Created by script.js`;
-  const notifImg = 'icons/apple-icon-180.png';
-  const options = {
-    body: notifBody,
-    icon: notifImg,
-  };
-
-  new Notification(notifBody, options);
-  setTimeout(randomNotification, 1000);
-}
-
 button.addEventListener('click', () => {
-  // console.log('hello');
-  // Notification.requestPermission().then((result) => {
-  //   if (result === 'granted') {
-  //     randomNotification();
-  //   }
-  // });
-  console.log('inside');
-
-  new Notification('Without permission');
-
+  // requires notifications to be enabled on device
   if (!('Notification' in window)) {
     // Check if the browser supports notifications
     alert('This browser does not support desktop notification');
   } else if (Notification.permission === 'granted') {
     // Check whether notification permissions have already been granted;
     // if so, create a notification
-    const notification = new Notification('Hi there!1');
-    new Notification('Hi there!2');
-
-    console.log('granted');
+    const notification = new Notification('Hi there!');
     // …
   } else if (Notification.permission !== 'denied') {
     // We need to ask the user for permission
@@ -70,3 +47,16 @@ button.addEventListener('click', () => {
   // At last, if the user has denied notifications, and you
   // want to be respectful there is no need to bother them anymore.
 });
+
+navigator.serviceWorker
+  .register('service-worker.js')
+  .then((registration) => {
+    return registration.pushManager
+      .getSubscription()
+      .then(async (subscription) => {
+        // registration part
+      });
+  })
+  .then((subscription) => {
+    // subscription part
+  });
