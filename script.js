5881b612e4dd9228cf24f3d63f4230e8d1b645fe
mxmnk <46030850+mxmnk@users.noreply.github.com>
2023-10-23 13:12:51 +0300

    Update PWA

--- script.js
+++ script.js
@@ -19,3 +19,54 @@ window.addEventListener('load', () => {
     navigator.serviceWorker.register('sw.js');
   }
 });
+
+const button = document.getElementById('notifications');
+
+function randomNotification() {
+  const notifBody = `Created by script.js`;
+  const notifImg = 'icons/apple-icon-180.png';
+  const options = {
+    body: notifBody,
+    icon: notifImg,
+  };
+
+  new Notification(notifBody, options);
+  setTimeout(randomNotification, 1000);
+}
+
+button.addEventListener('click', () => {
+  // console.log('hello');
+  // Notification.requestPermission().then((result) => {
+  //   if (result === 'granted') {
+  //     randomNotification();
+  //   }
+  // });
+  console.log('inside');
+
+  new Notification('Without permission');
+
+  if (!('Notification' in window)) {
+    // Check if the browser supports notifications
+    alert('This browser does not support desktop notification');
+  } else if (Notification.permission === 'granted') {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    const notification = new Notification('Hi there!1');
+    new Notification('Hi there!2');
+
+    console.log('granted');
+    // …
+  } else if (Notification.permission !== 'denied') {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === 'granted') {
+        const notification = new Notification('Hi there!');
+        // …
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
+});
